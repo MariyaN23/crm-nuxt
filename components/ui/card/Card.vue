032aef749ece22b2d11e8c41df42f6fcdd28224c
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2025-04-24 17:56:11 +0300

    board start card and types

---
+++ components/ui/card/Card.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import type {HTMLAttributes} from 'vue'
+import {cn} from '@/lib/utils'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <div
+      data-slot="card"
+      :class="
+      cn(
+        'bg-primary-foreground text-card-foreground flex flex-col gap-6 rounded-xl border border-transparent hover:border-border py-6 shadow-sm animation',
+        props.class,
+      )
+    "
+  >
+    <slot/>
+  </div>
+</template>
+
+<style scoped>
+@keyframes show {
+  from {
+    border: var(--border);
+    transform: scale(0.5) translateY(-30px);
+    opacity: 0.4;
+  }
+
+  90% {
+    border: var(--border);
+  }
+
+  to {
+    border: transparent;
+    transform: scale(1) translateY(0);
+    opacity: 1;
+  }
+}
+
+.animation {
+  animation: show 0.3s ease-in-out;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import type {ICard, IColumn} from "~/components/board/board.types";
 
+useSeoMeta({
+  title: 'Home | CRM System'
+})
+
+const dragCardRef = ref<ICard | null>(null)
+const sourceColumnRef = ref<IColumn | null>(null)
 </script>
 
 <template>
   <div class="p-10">
     <h1 class="mb-10">CRM System</h1>
+    <div>
+      <UiCard draggable="true">
+        <UiCardHeader role="button">name card</UiCardHeader>
+        <UiCardContent>Company</UiCardContent>
+        <UiCardFooter>Date</UiCardFooter>
+      </UiCard>
+    </div>
   </div>
 </template>
